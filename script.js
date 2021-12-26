085f83a3075568f6a306baf3404d57e3615a7751
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-12-26 15:29:35 -0600

    added baseline script.js file with comments

---
+++ script.js
@@ -0,0 +1,6 @@
+// A mobile and web application that can help healthcare professionals 
+// track and accurately chart a patient's Accutane history and dosage. This 
+// will calculate a patient's history with the medicine, what dose they will 
+// continue with, and when they will reach their cumulative goal dose. This 
+// application will save dermatology professionals time and headache while the patients 
+// will receive the proper treatment that they require. 
